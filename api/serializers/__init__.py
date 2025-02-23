57469a596d64592ec6b19ac5ddf9a6e633a3a838
lamadakwar@mail.tau.ac.il <lamadakwar@mail.tau.ac.il>
2025-02-24 05:01:18 +0530

    create order class and necessary files

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -2,3 +2,4 @@ from .furniture import FurnitureSerializer
 from .inventory import InventorySerializer
 from .user import UserSerializer
 from .shopping_cart import ShoppingCartSerializer
+from .order import OrderSerializer
