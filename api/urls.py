ad18f5cb000e6499390a015c74829370cc63fa2f
lamadakwar@mail.tau.ac.il <lamadakwar@mail.tau.ac.il>
2025-02-24 05:06:38 +0530

    checkout class creation

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,14 @@ from .views import InventoryViewSet
 from .views import RegisterUserView, LoginUserView, UserProfileView
 from .views import ShoppingCartViewSet
 from .views import OrderViewSet
+from .views import CheckoutViewSet
 
 router = DefaultRouter()
 router.register(r'furniture', FurnitureViewSet, basename='furniture')
 router.register(r'inventory', InventoryViewSet, basename='inventory')
 router.register(r'shopping-cart', ShoppingCartViewSet, basename='shopping-cart')
 router.register(r'order', OrderViewSet, basename='order')
+router.register(r'checkout', CheckoutViewSet, basename='checkout')
 
 urlpatterns = [
     path('', include(router.urls)),
