f5a917fd1104b72fe1ccc0007c7f9e16920714f1
naghambbb <Naghamb1@mail.tau.ac.il>
2025-02-23 22:31:17 +0530

    added User model, serializer & view

--- furniture_store/settings.py
+++ furniture_store/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'api.CustomUser'
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -125,3 +127,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
